a4c954bce2c64291cf0719d340f7b73c6ba08105
pylon2008 <pyliang_2008@163.com>
2013-06-24 09:19:45 +0800

    修改配置项。

--- TaobaoSetup.py
+++ TaobaoSetup.py
@@ -1,4 +1,46 @@
+#coding=GBK
 from distutils.core import setup
 import py2exe
 
-setup(console=["TaobaoViewer.py"])
+##options = {"py2exe":
+##
+##    {"compressed": 1, 
+##     "optimize": 2,
+##     "ascii": 1,
+##     "includes":includes,
+##     "bundle_files":  }
+##    }
+options = {"py2exe":
+           {"bundle_files": 1}
+           }
+
+data_files = [
+    ("", ["proxy.txt",
+          "UrlConfig.txt",
+          "C:\WINDOWS\system32\ole32.dll",
+          "C:\WINDOWS\system32\OLEAUT32.dll",
+          "C:\WINDOWS\system32\USER32.dll",
+          "C:\WINDOWS\system32\SHELL32.dll",
+          "C:\WINDOWS\system32\MSWSOCK.dll",
+          "C:\WINDOWS\system32\COMDLG32.dll",
+          "C:\WINDOWS\system32\COMCTL32.dll",
+          "C:\WINDOWS\system32\ADVAPI32.dll",
+          "c:\Python27\lib\site-packages\Pythonwin\mfc90.dll",
+          "C:\WINDOWS\system32\msvcrt.dll",
+          "C:\WINDOWS\system32\WS2_32.dll",
+          "C:\WINDOWS\system32\WINSPOOL.DRV",
+          "C:\WINDOWS\system32\GDI32.dll",
+          "C:\WINDOWS\system32\SHLWAPI.dll",
+          "C:\WINDOWS\system32\RPCRT4.dll",
+          "C:\WINDOWS\system32\VERSION.dll",
+          "C:\WINDOWS\system32\KERNEL32.dll",
+          "C:\WINDOWS\system32\\ntdll.dll"
+          ])
+    ]
+
+setup(
+    options = options,      
+    zipfile=None,
+    console=["TaobaoViewer.py"],
+    data_files=data_files,
+    )
