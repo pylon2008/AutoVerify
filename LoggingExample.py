da2d80241f0ed30af550ec89ac4f8153f9158f0d
pylon2008 <pyliang_2008@163.com>
2013-07-01 17:24:44 +0800

    添加收发邮件示例。

--- LoggingExample.py
+++ LoggingExample.py
@@ -1,6 +1,11 @@
 # coding=GBK
 import logging
 import logging.handlers
+import poplib   
+import email
+import smtplib 
+from email.mime.text import MIMEText 
+from email.header import Header      
 
 def multiHandle():
     logger = logging.getLogger("simple_example")
@@ -74,14 +79,84 @@ def emailHandle():
     logger.critical("critical message")
     
 def initLogging():
-##    LOG_FILENAME="logging.log"
-##    logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
-##    curTime = datetime.datetime.now
-##    strTime = str(curTime)
-##    logging.debug("===============================================Begin Log===============================================")
-    multiHandle()
+    LOG_FILENAME="logging.log"
+    logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
+    curTime = datetime.datetime.now
+    strTime = str(curTime)
+    logging.debug("===============================================Begin Log===============================================")
+
+
+def sendMail():
+    sender = '' 
+    receiver = '' 
+    subject = 'python email test' 
+    smtpserver = 'smtp.sina.com' 
+    username = '' 
+    password = '' 
+    
+    msg = MIMEText('你好','plain','utf-8')#中文需参数‘utf-8’，单字节字符不需要 
+    msg['Subject'] = Header(subject, 'utf-8')
+    msg['From'] = sender
+    msg['To'] = receiver
+     
+    smtp = smtplib.SMTP() 
+    smtp.connect('smtp.sina.com') 
+    smtp.login(username, password) 
+    smtp.sendmail(sender, receiver, msg.as_string()) 
+    smtp.quit()
+
+
+def showmessage(mail):
+    #print mail
+    if mail.is_multipart():
+        for part in mail.get_payload():
+            showmessage(part)
+    else:
+        type=mail.get_content_charset()
+        print "type: ", type
+        if type==None:
+            print mail.get_payload()
+        else:
+            try:
+                print unicode(mail.get_payload(),type)
+            except UnicodeDecodeError:
+                print mail
+
+def receMail():
+    host = 'pop.sina.com'  
+    username = '@sina.com'  
+    password = ''  
+      
+    pop_conn = poplib.POP3(host)   
+    pop_conn.user(username)   
+    pop_conn.pass_(password)   
+
+    mailCount,size=pop_conn.stat()
+    #Get messages from server:
+    messages = []
+    for i in range(1, mailCount+1):
+        msg = pop_conn.retr(i)
+        messages.append( msg )
+
+    for msg in messages:
+        allMsg = '\n'.join(msg[1])
+        mail = email.message_from_string(allMsg)
+        showmessage(mail)
+##        print "mail================================="
+##        subject = email.Header.decode_header(mail['subject'])[0][0]
+##        subcode = email.Header.decode_header(mail['subject'])[0][1]
+##        subStr = unicode(subject,subcode) 
+##
+##        print "Subject: ", subStr
+
+    pop_conn.quit()
+
+
 
 
 if __name__=='__main__':
-    #initLogging()
-    emailHandle()
+##    initLogging()
+##    multiHandle()
+##    emailHandle()
+##    sendMail()
+    receMail()
